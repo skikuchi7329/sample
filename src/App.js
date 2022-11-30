9a8b1dcbb53631f8c1f543ae72f22835a0221bf7
Shohei Kikuchi <killchance2256@gmail.com>
2022-12-01 04:18:32 +0900

    feat: Add roulette system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import './App.css';
-import Layout from './components/layout';
+import React, { useState } from 'react';
 
 function App() {
+  const [nameText, setNameText] = useState("");
+  const [nameList] = React.useState([]);
+
+  const onChangeNameText = (event) => {
+    setNameText(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    const newName = [
+      nameText,
+    ]
+    nameList.push(newName);
+  
+    setNameText("");
+  };
+
+  const roulette = () => {
+    const random = Math.floor(Math.random() * nameList.length);
+    document.getElementById("result").textContent = nameList[random];
+    if (nameList.length < 2) {
+      alert("error");
+    }
+    console.log(nameList)
+  }
+
   return (
-    <Layout />
-  );
+  <>
+    <div>
+      <h1>ルーレット</h1>
+      <input value={nameText} onChange={onChangeNameText} />
+      <button onClick={onClickAdd}>追加</button>
+    </div>
+    <div>
+      <ul>
+        {nameList.map(name=>
+          <li key={name}>{name}</li>
+        )}
+      </ul>
+    </div>
+    <div>
+      <button onClick={roulette}>スタート</button>
+      <p id="result">?</p>
+    </div>
+  </>
+  )
 }
 
 export default App;
